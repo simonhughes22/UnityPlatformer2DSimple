a84f7910c7f74504772750c4fea7323821d45a4b
SIMON HUGHES <simonhughes22@hotmail.com>
2022-02-07 00:26:24 -0600

    Add in other enemy type. Add common base class.

---
+++ Assets/Scripts/GameOverScreen.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScreen : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void Quit() {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();        
+    }
+}
